f19136ec45386edcd75f3c6ac1b345463ec28dc4
Filip <filip.tthuresson@gmail.com>
2021-02-13 11:51:58 +0100

    Added LocalStorage Support And also made it update old items/amounts

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ var output = document.getElementById('output');
 var items = [];
 var amounts = [];
 
+document.addEventListener("DOMContentLoaded",()=> {
+  var oldItems = JSON.parse(localStorage.getItem('items'));
+  var oldAmounts = JSON.parse(localStorage.getItem('amounts'))
+  if(oldItems != null){
+    oldItems.forEach((item, i) => {
+        output.innerHTML += `<div class="item"><h3>Item: ${item}</h3><p>Amount:${oldAmounts[i]}</p></div>`;
+    });
+  }
+});
+
 addbtn.addEventListener('click',()=>{
   if(input.value != null && !items.includes(input.value)){
     items.push(input.value);
     amounts.push(amount.value);
 
     output.innerHTML += `<div class="item"><h3>Item: ${input.value}</h3><p>Amount: ${amount.value}</p></div>`;
-    localStorage.setItem('list', items)
-    localStorage.setItem('amounts', amounts)
+    localStorage.setItem('items', JSON.stringify(items))
+    localStorage.setItem('amounts', JSON.stringify(amounts))
   }else if(items.includes(input.value)){
     alert("Item already added");
   }else{
@@ -24,4 +34,6 @@ addbtn.addEventListener('click',()=>{
 clear.addEventListener('click',()=>{
     output.innerHTML = "";
     items = [];
+    localStorage.removeItem('items');
+    localStorage.removeItem('amounts');
 });
